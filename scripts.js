5df52c4535a4344e9c2c0ae0c5dadd42273f5569
Rafael Alves <rafael.b.alvez@gmail.com>
2022-11-19 22:00:38 -0300

    added field error messages

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-function validateFields(){
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid; 
-    //Se o email for valido (verdadeiro), disabled recebe false (!true), ou seja, habilitado
+function OnChangeEmail(){
+    toggleButtonsDisable();
+    toggleEmailErros();
+    
+}
 
-    const passwordValid = isPassWordValid();
-    document.getElementById("login-button").disabled = !passwordValid || !emailValid;
-    //o botão de login recebe disabled = true (desabilitado) quando passwordValid = false OU emailValid = false
+function OnChangePassword(){
+    toggleButtonsDisable();
+    togglePasswordErrors();
 }
 
 function isEmailValid(){
@@ -23,6 +24,40 @@ function isPassWordValid(){
         return false;
     }
     return true;
+}
+
+function toggleEmailErros(){
+    const email = document.getElementById("email").value;
+    if(!email){
+        document.getElementById("email-required-error").style.display = "block";
+    }else{
+        document.getElementById("email-required-error").style.display = "none";
+    }
+
+    if(validateEmail(email)){
+        document.getElementById("email-invalid-error").style.display = "none";
+    }else{
+        document.getElementById("email-invalid-error").style.display = "block";
+    }
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById("password").value;
+    if(!password){
+        document.getElementById("password-required-error").style.display = "block";
+    }else{
+        document.getElementById("password-required-error").style.display = "none";
+    }
+}
+
+function toggleButtonsDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid; 
+    //Se o email for valido (verdadeiro), disabled recebe false (!true), ou seja, habilitado
+
+    const passwordValid = isPassWordValid();
+    document.getElementById("login-button").disabled = !passwordValid || !emailValid;
+    //o botão de login recebe disabled = true (desabilitado) quando passwordValid = false OU emailValid = false
 }
 
 function validateEmail(email) {
