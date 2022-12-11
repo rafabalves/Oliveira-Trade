c6fe9d8820f9dbb8e73afcb654490b6612ee475d
Rafael Alves <rafael.b.alvez@gmail.com>
2022-12-10 21:23:24 -0300

    add user card field values ​​with javascript

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,3 +5,31 @@ function logout(){
         alert("Erro ao fazer logout");
     })
 }
+
+const fakeCard = {
+    name: "Rafael Alves",
+    email: "rafael.balvez@gmail.com",
+    birthDate: "1997-10-14",
+    gender: "Masculino"
+}
+
+
+function findUserData(){
+    addUserDataToScreen(fakeCard);
+    
+}
+
+function addUserDataToScreen(userData){
+    document.getElementById("name").innerHTML = userData.name;
+    document.getElementById("email").innerHTML = userData.email;
+    document.getElementById("birthDate").innerHTML = formatDate(userData.birthDate);
+    document.getElementById("gender").innerHTML = userData.gender;
+}
+
+function formatDate(date){
+    const splittedDate = date.split("-"); 
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+}
+
+findUserData();
+
